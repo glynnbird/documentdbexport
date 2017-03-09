1e49d97c022c5ff9943a4152e6758e6756c9417e
Glynn Bird <glynn.bird@gmail.com>
2017-03-09 17:20:40 +0000

    1.0.1 - fix count

--- index.js
+++ index.js
@@ -2,29 +2,50 @@
 var documentClient = require('documentdb').DocumentClient;
 var async = require('async');
 
+// export a whole collection
 var exportCollection = function(endpoint, key, database, collection, callback) {
+
+  // record start time
   var start = new Date().getTime();
+
+  // initialise library
   var client = new documentClient(endpoint, { masterKey: key });
-  var collection_url = 'dbs/' + database + '/colls/' + collection;
-  var iterator = client.queryDocuments(collection_url);
+
+  // start the query
   var lastItem = null;
   var i = 0;
+  var collection_url = 'dbs/' + database + '/colls/' + collection;
+  var iterator = client.queryDocuments(collection_url);
+
+  // iterate through the collection
   async.doUntil(function(cb) {
+
+    // retrieve the next item
     iterator.nextItem(function(err, element) {
+
+      // if it's valid
       if (element) {
+
+        // remove the DocumentDB special bits
         delete element._ts;
         delete element._etag;
         delete element._attachments;
         delete element._self;
         delete element._rid;
+        i++;
+
+        // output the JSON
         console.log(JSON.stringify(element));
       }
-      lastItem = element
+      lastItem = element;
       cb(null, null);
     });
   },function() {
+    // continue until we get 'undefined'
     return lastItem === undefined;
   }, function(err, results) {
+
+    // call the callback with a summary
     var end = new Date().getTime();
     callback(err, { records: i, time: (end - start)/1000})
   });
